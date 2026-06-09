f05cb8fdd74433208e3bee95484b215be340c400
Tafsir2015 <readsolv@gmail.com>
2026-06-09 15:29:27 +0600

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const mineflayer = require('mineflayer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3000; 
+
+app.get('/start-bot', (req, res) => {
+    const bot = mineflayer.createBot({
+        host: 't-survivalmc.aternos.me',
+        port: 25565,
+        username: 'HopWeb_Bot'
+    });
+
+    bot.on('spawn', () => {
+        res.send('Bot Joined The Game');
+    });
+
+    bot.on('error', (err) => {
+        res.status(500).send('Failed To Join');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
